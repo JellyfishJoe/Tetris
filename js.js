32b39e8e17e48202fc52297a8c9b02373f160931
JellyfishJoe <jellyfishjoe5@gmail.com>
2019-05-14 15:16:46 -0700

    asdffggh

--- js.js
+++ js.js
@@ -8,16 +8,17 @@ let squareW = canvas.width / gridSize;
 
 let gameGrid = [];
 
-let rightl = [{x: 5, y: 7, free: true}, {x: 6, y: 7, free: true}, {x: 5, y: 6, free: false}, {x: 5, y: 5, free: false}];
-let leftl = [];
-let square = [];
-let rightz = [];
-let leftz = [];
-let line = [];
-
-let shapes = [rightl, leftl, square, rightz, leftz, line]
-
-let curBlock = shapes[0];
+let rightl = [{x: 9, y: 7}, {x: 9, y: 5}, {x: 9, y: 6}, {x: 10, y: 7}];
+let leftl = [{x: 9, y: 7}, {x: 9, y: 5}, {x: 9, y: 6}, {x: 8, y: 7}];
+let square = [{x: 9, y: 7}, {x: 8, y: 7}, {x: 8, y: 8}, {x: 9, y: 8}];
+let rightz = [{x: 9, y: 7}, {x: 9, y: 6}, {x: 10, y: 6}, {x: 8, y: 7}];
+let leftz = [{x: 9, y: 7}, {x: 8, y: 6}, {x: 9, y: 6}, {x: 10, y: 7}];
+let line = [{x: 9, y: 7}, {x: 9, y: 5}, {x: 9, y: 6}, {x: 9, y: 8}];
+let tee = [{x: 9, y: 7}, {x: 8, y: 7}, {x: 10, y: 7}, {x: 9, y: 8}];
+let shapes = [rightl, leftl, square, rightz, leftz, line, tee];
+let curBlock = shapes[2];
+let index = shapes.indexOf(curBlock);
+console.log(index);
 
 ctx.fillStyle = '#ff0000';
 
@@ -53,33 +54,54 @@ document.addEventListener('keydown', function(event){
 			gameLoop();
 			break;
 		case 65:
-			slideCur(-1);
+			slideCur(curBlock, -1); //move left
 			break;
 		case 68:
-			slideCur(1);
+			slideCur(curBlock, 1); //move right
 			break;
-		case 81:
+		case 67:
 			chooseShape();
+			break;
+		case 90:
+			index += 1;
+			if(index > shapes.length - 1){
+				index = 0;
+			}
+			curBlock = shapes[index];
+			drawCurrentBlock(curBlock);
+			break;
+		case 88:
+			index -= 1;
+			if(index < 0){
+				index = shapes.length - 1;
+			}
+			curBlock = shapes[index];
+			drawCurrentBlock(curBlock);
 			break;
 	}
 });
 
-function slideCur(dir){
-
+function slideCur(cur, dir){
+	console.log(dir);
+	cur.forEach(function(block){
+		let newX = block.x + dir;
+		if(gameGrid[block.y][newX] == 0){
+			block.x = newX;
+		};
+	});
+	drawCurrentBlock(curBlock);
+	freenessUpdate(curBlock);
 }
 
 function rotate(cur){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	//drawGrid();
 	let affMat = [0,-1,
 				  1, 0];
 	for(i = 1; i < cur.length; i ++){
-		gameGrid[cur[i].y][cur[i].x] = 0;
 		x = cur[i].x - cur[0].x;
 		y = cur[i].y - cur[0].y;
 		cur[i].x = x*affMat[0] + y*affMat[1] + cur[0].x;
 		cur[i].y = x*affMat[2] + y*affMat[3] + cur[0].y;
-		gameGrid[cur[i].y][cur[i].x] = 1;
 	}
 	ctx.fillStyle = 'red';
 	drawCurrentBlock(curBlock);
@@ -90,7 +112,7 @@ function rotate(cur){
 function freenessUpdate(cur){
 	for(i = 0; i < cur.length; i++){
 		for(j = 0; j < cur.length; j++){
-			if(cur[i].x = cur[j].x && cur[i].y + 1 == cur[j].y){
+			if(cur[i].x == cur[j].x && cur[i].y + 1 == cur[j].y){
 				cur[i].free = false;
 			} else{
 				cur[i].free = true;
@@ -99,52 +121,46 @@ function freenessUpdate(cur){
 	}
 }
 
-function collisionCheck(cur){
-	console.log('check');
+function collisionCheck(cur, mx, my){
 	for(i = 0; i < cur.length; i++){
-		if(cur[i].free == true){
-			console.log('true');
-			console.log(gameGrid[cur[i].y + 1][cur[i].x] == 1);
-			console.log(gameGrid[cur[i].y + 1][cur[i].x] > gridSize);
-			if(cur[i].y + 1 > gridSize || gameGrid[cur[i].y + 1][cur[i].x] == 1){
-				console.log('blocked');
+		if(cur[i].y + my < gridSize && cur[i].x + mx < gridSize && cur[i].x + mx > 0){
+			//console.log(gameGrid[cur[i].y + 1][cur[i].x] == 1);
+			//console.log(gameGrid[cur[i].y + 1][cur[i].x] > gridSize);
+			if(gameGrid[cur[i].y + my][cur[i].x] == 1){
+				blockStay();
 				return true;
 			} else{
 				return false;
 			}
+		} else{
+			return true;
 		}
 	}
 }
 
-function chooseShape(){
-	let rand = Math.floor(Math.random*5);
+function blockStay(){
+	zz
 }
 
-function addShape(cur){
-	console.log(gameGrid);
-	cur.forEach(function(block){
-		gameGrid[block.y][block.x] = 1;
-	});
-	drawCurrentBlock(cur);
+function chooseShape(){
+	let rand = Math.floor(Math.random()*6);
+	curBlock = shapes[rand];
+	let index = shapes.indexOf(curBlock);
+	drawCurrentBlock(curBlock);
 }
 
 function drawCurrentBlock(cur){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	cur.forEach(function(block){
 		ctx.fillRect(squareW*block.x, squareH*block.y, squareW, squareH);
 	});
 }
 
 function gameLoop(){
-	console.log('game run');
-	if(!collisionCheck(curBlock)){
-		curBlock.forEach(function(block){
-			gameGrid[block.y][block.x] = 0
-		});
+	if(!collisionCheck(curBlock, 0, 1)){
 		curBlock.forEach(function(block){
 			block.y += 1;
-			gameGrid[block.y][block.x] = 1
 		});
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		//drawGrid();
 		drawCurrentBlock(curBlock);
 		console.log(gameGrid);
@@ -153,6 +169,6 @@ function gameLoop(){
 	}
 }
 
-addShape(curBlock);
+drawCurrentBlock(curBlock);
 
 //let gameInterval = setInterval(gameLoop, 1000);
